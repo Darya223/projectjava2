f56dd6a9db0d26e195603f094de94e85ed2036e8
Сексуальный пингвин <ghh@mail.ru>
2018-09-02 17:43:53 +0300

    one

--- src/chasy/TestOne.java
+++ src/chasy/TestOne.java
@@ -3,7 +3,9 @@ package chasy;
 public class TestOne {
     public static void main(String[] args) {
         int t = 1865;
-        System.out.println("Было дано " + t + " часов");
-        System.out.println("Это равно" + (t / 24) + "дней" + " и" + (t - (77 * 24)) + " часов");
+        int s = 24;
+        int y = t%s;
+        int x = (t-y)/s;
+        System.out.println("Было дано 1865 часов." + "Это равно" + x + "дней и" + y + "часов");
     }
 }
